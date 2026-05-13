03106cf4ba2898e77e3a40529d3bebfd293b3313
kiran <kiranvaswani88788@gmail.com>
2026-05-13 19:16:17 +0530

    Saved books

--- book.rb
+++ book.rb
@@ -1,6 +1,7 @@
+require "json"
 class Book
   attr_accessor  :author_name ,:book_details
-  
+
   def initialize(author_name,book_details)
     @author_name=author_name
     @book_details=book_details
@@ -8,9 +9,18 @@ class Book
 
   def validate_book_details
     if((author_name != "" && book_details != "") && (author_name.length>=2 && book_details.length>=2))
-       puts "if successfully"
+       save_book
     else
       puts "hello else"
     end 
   end
+
+  def save_book
+    hash={author_name: "#{author_name}", book_details: "#{book_details}"}
+    File.open("book_store.txt","a") do |file|
+      file.puts(JSON.pretty_generate(hash))
+      puts "data save successfuly"
+    end
+  end
+
 end
